d9f83a0f8cdc55909d3bf3f5f3dadbc8fcfe0770
Ed Gleeck <edrebg@gmail.com>
2020-12-28 18:02:35 -0500

    only on branch

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Changing here!
+          Learn React! Testing this new feature!
         </a>
       </header>
     </div>
